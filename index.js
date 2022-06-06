2430538194c6f12244a4f462be860d3552de8392
Magezi <arafats144@gmail.com>
2022-06-06 18:20:52 +0300

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const expressSession = require('express-session')({
 
   
 //Defining variables, routes and models
-const config = require('./config/database');
+// const config = require('./config/database');
 const loginPage = require('./routes/loginPage');
 const inmateRegister = require('./routes/inmateRegist');
 const userRegister = require('./routes/register');
@@ -24,17 +24,17 @@ const inmatesReport = require('./routes/inmatesReport');
 const application = express();
 
 //Establishing connection to the database
-mongoose.connect(config.database);
-const db = mongoose.connection;
+// mongoose.connect(config.database);
+// const db = mongoose.connection;
 
 //Testing the connection to the database
-db.once('open', () => {
-    console.log('Successfully connected to the Database');
-});
+// db.once('open', () => {
+//     console.log('Successfully connected to the Database');
+// });
 
-db.on('error', (err) => {
-    console.error(err);
-});
+// db.on('error', (err) => {
+//     console.error(err);
+// });
 
 //Setting the view engine
 application.engine('pug', require('pug').__express);
